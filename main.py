6dbe8906a09e7d8095da0dce0c4a7cb028284fc9
raaedahmed23 <raaedahmed23@gmail.com>
2023-11-29 00:47:29 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def spash_screen():
+    print("*" * 50)
+    print("Welcome to DavisBase CLI")
+    print("*" * 50)
+
+    usr_input = input("davisql>")
+    print(usr_input)
+
+
+if __name__ == "__main__":
+    spash_screen()
